9b7a4aead16e81cc4cb7e99c5b2e30bd0095ebe8
Johandros <hansjuergoe@gmail.com>
2022-11-07 18:12:52 +0100

    hardware_sim command line arguments added

--- practice_tasks/hooked/A_Hardware_Simulator/solution/standard/hardware_sim.py
+++ practice_tasks/hooked/A_Hardware_Simulator/solution/standard/hardware_sim.py
@@ -1,11 +1,20 @@
 #!/usr/bin/python3
 
+import os
+import sys
 from simulator import Simulator
 
 
 def main():
+    simulate_file = "./asm_codes/asm_codes_1.asm"
+    if len(sys.argv) > 1:
+        if os.path.exists(sys.argv[1] and os.path.isfile(sys.argv[1])):
+            simulate_file = sys.argv[1]
+        else:
+            print("Invalid file path:", sys.argv[1])
+
     sim = Simulator()
-    sim.simulate_asm_file("./asm_codes/asm_codes_1.asm")
+    sim.simulate_asm_file(simulate_file)
 
 
 main()
