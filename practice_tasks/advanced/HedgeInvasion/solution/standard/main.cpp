5b4199aea331cbb948b29ec191fb269988f8de0c
Johandros <kochjohannes98@gmail.com>
2023-07-06 16:56:03 +0200

    HedgeInvasion naming fix

--- practice_tasks/advanced/HedgeInvasion/solution/standard/main.cpp
+++ practice_tasks/advanced/HedgeInvasion/solution/standard/main.cpp
@@ -10,7 +10,7 @@ int main(int argc, char *argv[])
     QObject::connect(&engine, &QQmlApplicationEngine::objectCreationFailed,
         &app, []() { QCoreApplication::exit(-1); },
         Qt::QueuedConnection);
-    engine.loadFromModule("ZombieInvasion", "Main");
+    engine.loadFromModule("HedgeInvasion", "Main");
 
     return app.exec();
 }
